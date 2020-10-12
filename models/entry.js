bc298b9ceb954a322cd9a9dec91afd24ddda4457
Anthony Pearo <anthony.pearo@colorado.edu>
2020-10-12 16:03:40 -0600

    added some field entries

--- models/entry.js
+++ models/entry.js
@@ -21,8 +21,25 @@ module.exports = function(sequelize, DataTypes) {
             "Please enter your last name with at least 1 character but no more than 255."
         }
       }
-    }
+    },
+    /*
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      //validate: {}
+    }*/
 
+    airline: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [1, 255],
+          msg:
+              "Please enter your airline with at least 1 character but no more than 255."
+        }
+      }
+    }
   });
   return Entry;
 };
--- view/entry.js
+++ view/entry.js
@@ -1,10 +1,22 @@
+// Need to finish mapping from index.html to models/entry.js and view/entry.js
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("submitAnswer").addEventListener('click', event => {
     console.log("inside entry.js 1")
 
     let newEntry = {
+      //text entries
       firstname: document.getElementById("fname").value,
-      lastname: document.getElementById("lname").value
+      lastname: document.getElementById("lname").value,
+      //date not working yet
+      //date: document.getElementById("date").value,
+      airline: document.getElementById("airline").value,
+      //checkboxes ?? values for these?
+      /*
+      sympt1: document.getElementById("sympt1").value,
+      sympt2: document.getElementById("sympt2").value,
+      sympt3: document.getElementById("sympt3").value,
+      //radio ?? no id so name?
+      diagnosis: document.getElementsByName("choice").value */
     }
 
     console.log("newEntry: ", newEntry);
