8eb26136a150604b302ade9f0fddeb024f2e9338
Amanda Killeen <amanda.killeen@gmail.com>
2020-10-11 18:00:52 -0700

    updated schema.sql with table creation methods.

--- DBCreationScript.sql
+++ DBCreationScript.sql
@@ -1,34 +1,51 @@
-CREATE DATABASE bewell_db;
+CREATE DATABASE IF NOT EXISTS bewell_db;
 
 USE bewell_db;
 
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE IF NOT EXISTS `user`
 (userID INTEGER NOT NULL AUTO_INCREMENT
 , firstname VARCHAR(45) NOT NULL
 , lastname VARCHAR(45) NOT NULL
-, userSubmissionDate DATE NOT NULL
 , PRIMARY KEY (userID)
 );
 
 CREATE TABLE IF NOT EXISTS illness
 (illnessID INTEGER NOT NULL AUTO_INCREMENT
-, illness VARCHAR(45) NOT NULL
+, illnessName VARCHAR(45) NOT NULL
 , PRIMARY KEY (illnessID)
 );
 
-CREATE TABLE IF NOT EXISTS user_illness
-(userID INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS flight
+(flightID INTEGER NOT NULL AUTO_INCREMENT
+, flightDate DATE NOT NULL
+, airline VARCHAR(45) NOT NULL
+, flightNumber VARCHAR(45) NOT NULL
+, PRIMARY KEY (flightID)
+);
+
+CREATE TABLE IF NOT EXISTS symptom
+(symptomID INTEGER NOT NULL
+, symptomName VARCHAR(45) NOT NULL
+, PRIMARY KEY (symptomID)
+);
+
+CREATE TABLE IF NOT EXISTS submission
+(submissionID INTEGER NOT NULL AUTO_INCREMENT
+, userID INTEGER NOT NULL
+, flightID INTEGER NOT NULL
 , illnessID INTEGER NOT NULL
+, submissionDate DATE NOT NULL
+, PRIMARY KEY (submissionID)
 , FOREIGN KEY (userID) REFERENCES users(userID)
+, FOREIGN KEY (flightID) REFERENCES flight(flightID)
 , FOREIGN KEY (illnessID) REFERENCES illness(illnessID)
 );
 
-CREATE TABLE IF NOT EXISTS flights
-(flightID INTEGER NOT NULL AUTO_INCREMENT
-, airline VARCHAR(45) NOT NULL
-, flightNumber VARCHAR(45) NOT NULL
-, flightDate DATE NOT NULL
-, illnessID INTEGER NOT NULL
-, PRIMARY KEY (flightID)
-, FOREIGN KEY (illnessID) REFERENCES illness(illnessID)
+CREATE TABLE IF NOT EXISTS submissionSymptom
+(submissionSymptonID INTEGER NOT NULL AUTO_INCREMENT
+, submissionID INTEGER NOT NULL
+, symptomID INTEGER NOT NULL
+, PRIMARY KEY (submissionSymptonID)
+, FOREIGN KEY (submissionID) REFERENCES submission(submissionID)
+, FOREIGN KEY (symptomID) REFERENCES symptom(symptomID)
 );
