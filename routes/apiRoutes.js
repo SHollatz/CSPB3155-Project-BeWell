1907682e962982e2971b9d572095c89a2948e9ef
Amanda Killeen <amanda.killeen@gmail.com>
2020-10-19 15:29:33 -0700

    started to update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,8 +9,18 @@ module.exports = function(app) {
   });
 
   app.post("/api/entry", function(req, res) {
+    let user = { //object
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    }
+    //breakdown incoming requests into database models
+    //check if user already exists by email, grab id, else create new user, grab id
+    //find Flight, get flightID
+    //insert into submission, get submissionID
+    //insert into submissionSymptom
     db.Entry.create(req.body).then(function(dbEntry) {
       res.json(dbEntry);
     });
   });
 };
+//Route that pulls back illnesses, symptoms, flights, list of aggregated submissions
